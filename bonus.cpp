db9148bb8143e4c244590ec3018d8aa147ee3182
jamesjaw <50652557+jamesjaw@users.noreply.github.com>
2021-12-17 00:41:32 +0800

    Update bonus.cpp

--- bonus.cpp
+++ bonus.cpp
@@ -153,7 +153,7 @@ int main(int argc,char* argv[]){
         for(int j=0;j<address_bits;j++){
             bit[j] = add % 10;
             add = add / 10;
-            if(j== offset_bit_count + indexing_bit_count - 1)
+            if(j== offset_bit_count - 1)
                 tag = add;
         }
         
